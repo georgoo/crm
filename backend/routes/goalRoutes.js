e59f31f58b0152ae31c582fb9bf2185092190607
George Paul <georgepaulkp@gmail.com>
2023-05-06 10:19:05 +0530

    first commit

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals, deleteGoal, updateGoal, setGoal } =require('../controllers/goalController')
+
+const {protect} =require('../middleware/authMiddleware')
+router.route('/').get(protect,getGoals).post(protect,setGoal)
+
+router.route('/:id').put(protect,updateGoal).delete(protect,deleteGoal)
+
+
+module.exports = router
